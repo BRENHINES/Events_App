800b78b74c3466fe8d469765d00897f15cb43027
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2023-12-11 05:07:58 -0500

    FEAT: Ajout de la Splash Screen

--- app/src/main/java/com/example/event_app/presentation/LoginChoice.kt
+++ app/src/main/java/com/example/event_app/presentation/LoginChoice.kt
@@ -2,11 +2,13 @@ package com.example.event_app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.event_app.R
 
 class LoginChoice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.login_choice)
     }
 }
