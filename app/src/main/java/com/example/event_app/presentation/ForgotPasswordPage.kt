2d1644817d267442b779f3e084f5c0f3db46db94
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2023-12-11 06:18:33 -0500

    FEAT: Ajout de la page de forget password

---
+++ app/src/main/java/com/example/event_app/presentation/ForgotPasswordPage.kt
@@ -0,0 +1,12 @@
+package com.example.event_app.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.event_app.R
+
+class ForgotPasswordPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forgot_password_page)
+    }
+}
