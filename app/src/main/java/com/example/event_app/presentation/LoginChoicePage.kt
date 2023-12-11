cf8953af7dc0f8728bf599dc720c6ac620667d0e
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2023-12-11 05:41:04 -0500

    FEAT: Ajout de la page de connexion

---
+++ app/src/main/java/com/example/event_app/presentation/LoginChoicePage.kt
@@ -0,0 +1,14 @@
+package com.example.event_app.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.event_app.R
+
+class LoginChoicePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.login_choice_page)
+    }
+}
--- app/src/main/java/com/example/event_app/presentation/LoginPage.kt
+++ app/src/main/java/com/example/event_app/presentation/LoginPage.kt
@@ -2,13 +2,11 @@ package com.example.event_app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.event_app.R
 
-class LoginChoice : AppCompatActivity() {
+class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        setContentView(R.layout.login_choice)
+        setContentView(R.layout.login_page)
     }
 }
