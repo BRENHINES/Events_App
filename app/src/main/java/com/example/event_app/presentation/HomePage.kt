561854dff3f4cae7e447c220deef4956a850be81
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2023-12-12 09:00:36 -0500

    FEAT: Ajout de la page de choix de préférence, de la page d' acceuil et des différents fragments

---
+++ app/src/main/java/com/example/event_app/presentation/HomePage.kt
@@ -0,0 +1,52 @@
+package com.example.event_app.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.event_app.R
+import com.example.event_app.databinding.HomePageBinding
+import com.example.event_app.presentation.fragments.AddFragment
+import com.example.event_app.presentation.fragments.HomeFragment
+import com.example.event_app.presentation.fragments.NotificationsFragment
+import com.example.event_app.presentation.fragments.ProfileFragment
+import com.example.event_app.presentation.fragments.TicketFragment
+
+class HomePage : AppCompatActivity() {
+
+    private lateinit var binding : HomePageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Making binding in the Home Page
+        binding = HomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set a default page to show when we open the app
+        replaceFragment(HomeFragment())
+
+        // Set the button click to change fragments page
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.navHome -> replaceFragment(HomeFragment())
+                R.id.navTicket -> replaceFragment(TicketFragment())
+                R.id.navAdd -> replaceFragment(AddFragment())
+                R.id.navNotification -> replaceFragment(NotificationsFragment())
+                R.id.navProfile -> replaceFragment(ProfileFragment())
+
+                else -> {  }
+            }
+            true
+        }
+    }
+
+    /* replaceFragment function to replace
+    fragment when we click on the icon
+    button on the bottom nav bar */
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
